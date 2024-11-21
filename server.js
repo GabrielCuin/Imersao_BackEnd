2517d69c523efaa772182ef3328da80957ae4502
Gabriel Cuin <gabrielcuinprof@gmail.com>
2024-11-21 00:55:20 -0300

    aula 3, com problemas na conexão

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
+import routes from "./src/routes/PostsRoutes.js";
 
 const app = express();
+
+routes(app);
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/api", (req, res) => {
-    res.status(200).send("Rota inicial");
 });
